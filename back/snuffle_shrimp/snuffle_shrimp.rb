40d05535da590ea8894dfec8bb3def8da2aa26d8
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-06-21 00:43:18 -0700

    remember the shrimp's name

--- back/snuffle_shrimp/snuffle_shrimp.rb
+++ back/snuffle_shrimp/snuffle_shrimp.rb
@@ -1,7 +1,15 @@
 class SnuffleShrimp
+  def initialize(name)
+    @name = name
+  end
+
   def capture_snuffle_information(*args)
   end
 
   def captured_snuffle_information
+  end
+
+  def to_s
+    @name
   end
 end
